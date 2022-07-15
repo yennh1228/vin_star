96deac5cc45ee39fa82de26325866d0e25a641d8
yennh1228 <yennh.ftu@gmail.com>
2022-07-15 10:19:34 +0700

    creat new Sign Up Page

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: SignInPage(),
     );
   }
--- lib/signInPage.dart
+++ lib/signInPage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:vin_star/material/buildPass.dart';
+import 'package:vin_star/material/buildPhoneNo.dart';
+import 'package:vin_star/material/hotLine.dart';
+import 'package:vin_star/material/signInButton.dart';
+import 'package:vin_star/material/signUpButton.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -25,102 +30,22 @@ class _SignInPageState extends State<SignInPage> {
               child: Image.asset("assets/images/vinstar_logo.png"),
             ),
             const SizedBox(height: 20),
-            buildPhoneNo(),
+            BuildPhoneNo(),
             const SizedBox(height: 20),
-            buildPass(),
+            const BuildPass(),
             const SizedBox(height: 40),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                signUp(),
-                signIn(),
+              children: const [
+                SignUpButton(),
+                SignInButton(),
               ],
             ),
             const SizedBox(height: 250),
-            hotLine()
+            // HotLine()
           ]),
         ),
       ),
     ));
-  }
-
-  TextField buildPhoneNo() => TextField(
-        controller: phoneController,
-        keyboardType: TextInputType.number,
-        textInputAction: TextInputAction.done,
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          labelText: 'Số điện thoại',
-          hintText: '0123456789',
-          suffixIcon: phoneController.text.isEmpty
-              ? Container(width: 0)
-              : IconButton(
-                  onPressed: () {
-                    return phoneController.clear();
-                  },
-                  icon: const Icon(Icons.close),
-                ),
-        ),
-      );
-
-  TextField buildPass() => TextField(
-        keyboardType: TextInputType.emailAddress,
-        textInputAction: TextInputAction.done,
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          labelText: 'Mật khẩu',
-          hintText: 'Mật khẩu',
-          suffixIcon: IconButton(
-              icon: isPasswordVisible
-                  ? const Icon(Icons.visibility_off)
-                  : const Icon(Icons.visibility),
-              onPressed: () =>
-                  setState(() => isPasswordVisible = !isPasswordVisible)),
-        ),
-        obscureText: isPasswordVisible,
-      );
-
-  TextButton signUp() {
-    return TextButton(
-      onPressed: () {
-        print("Đăng ký");
-      },
-      child: const Text(
-        "Đăng ký",
-        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-
-  ElevatedButton signIn() {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: const Color(0xff009688),
-      ),
-      onPressed: () {
-        print("Đăng nhập");
-      },
-      child: const Text(
-        "Đăng nhập",
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-
-  RichText hotLine() {
-    return RichText(
-      text: const TextSpan(
-        text: 'HotLine: ',
-        style: TextStyle(
-          fontSize: 18,
-          color: Colors.black,
-        ),
-        children: <TextSpan>[
-          TextSpan(
-              text: '1800.1186',
-              style: TextStyle(fontSize: 16, color: Color(0xff009688))),
-        ],
-      ),
-    );
   }
 }
